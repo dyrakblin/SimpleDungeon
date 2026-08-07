88a3ac1acfea3500e3907b65c7dc799b750bdf7c
dyrakblin <kuceravcenkonikolaj@gmail.com>
2026-08-07 04:42:17 +0300

    Добавьте файлы проекта.

---
+++ SimpleDungeon/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace SimpleDungeon
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("-----Главное меню-----");
+            Console.WriteLine("1 - начать");
+            Console.WriteLine("2 - выход");
+            Console.Write("Ввод:");
+            Console.ReadKey();
+        }
+    }
+}
