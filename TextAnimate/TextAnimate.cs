db3ad0f9279d752486559a9f3e73c9a0ffcf5069
dyrakblin <kuceravcenkonikolaj@gmail.com>
2026-08-09 20:55:03 +0300

    Добавлены анимации текста.

---
+++ TextAnimate/TextAnimate.cs
@@ -0,0 +1,25 @@
+﻿namespace TextAnimate
+{
+
+    public static class Anim
+    {
+        public static void Type(string text, int DelayMs = 0)
+        {
+            foreach (char c in text)
+            {
+                Console.Write(c);
+                Thread.Sleep(30);
+            }
+            if (DelayMs > 0)
+            {
+                Thread.Sleep(DelayMs);
+            }
+        }
+    
+        public static void Delay(string text, int DelayMs)
+        {
+            Console.WriteLine(text);
+            Thread.Sleep(DelayMs);
+        }
+    }
+}
